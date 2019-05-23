2de1230bf1bdfc190d94183854932528091c3ccd
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-05-23 14:38:40 +0200

    Fix menu for mobile

--- client/src/components/Layout/Header/Header.js
+++ client/src/components/Layout/Header/Header.js
@@ -5,37 +5,51 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // Import styles
 import './Header.scss'
 
+// Import logo
+import logo from '../../../assets/images/logo.jpg'
+console.log(logo);
+
+
 class Header extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            collapsed: false
+        };
     }
+
 
     render() {
         return (
             <div className="d-flex">
-                <div className="col-lg-4 col-sm-12 text-center">
-                    <h1>Jubiko</h1>
+                <div className="col-sm-4 ">
+                    <img src={logo} alt="Jubiko" className="float-left" />
                 </div>
-                <div className="col-lg-8 col-sm-12 d-flex justify-content-end">
-                    <ul class="nav nav-pills">
-                        <li class="nav-item">
-                            <NavLink to="/" activeClassName="active" className="nav-link" exact={true}>Home</NavLink>
-                        </li>
-                        <li class="nav-item">
-                            <NavLink to="/faq" activeClassName="active" className="nav-link" exact={true}>FAQ</NavLink>
-                        </li>
-                        <li class="nav-item">
-                            <NavLink to="/regulamin" activeClassName="active" className="nav-link" exact={true} >Regulamin</NavLink>
-                        </li>
-                        <li class="nav-item">
-                            <NavLink to="/kontakt" activeClassName="active" className="nav-link" exact={true}>Kontakt</NavLink>
-                        </li>
-                        <li class="nav-item">
-                            <NavLink to="/koszyk" activeClassName="active" className="nav-link" exact={true} ><i><FontAwesomeIcon icon="shopping-bag" /></i></NavLink>
-                        </li>
-                    </ul>
-
+                <div className="col-sm-8 d-flex justify-content-end">
+                    <nav className="navbar navbar-expand-md navbar-light">
+                        <button className="navbar-toggler pull-right" onClick={state => this.setState({ collapsed: this.state.collapsed ? false : true })}>
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className={this.state.collapsed ? "collapse navbar-collapse bg-light show " : "collapse navbar-collapse"}>
+                            <ul className="nav nav-pills flex-column flex-md-row align-items-center">
+                                <li className="nav-item">
+                                    <NavLink to="/" activeClassName="active" className="nav-link" exact={true}>Home</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink to="/faq" activeClassName="active" className="nav-link" exact={true}>FAQ</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink to="/regulamin" activeClassName="active" className="nav-link" exact={true} >Regulamin</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink to="/kontakt" activeClassName="active" className="nav-link" exact={true}>Kontakt</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink to="/koszyk" activeClassName="active" className="nav-link" exact={true} ><i><FontAwesomeIcon icon="shopping-bag" /></i></NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
                 </div>
             </div>
         );

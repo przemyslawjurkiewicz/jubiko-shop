859f38b575e912315d2ae795a5e852d67f4d762f
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-05-24 10:58:34 +0200

    Add products carts

---
+++ client/src/assets/Products.js
@@ -0,0 +1,39 @@
+const Products = (
+    [
+        {
+            "name": "Tytanowe kolczyki - Dżungla",
+            "price": 63.00,
+            "description": "Kolczyki wykonane są ręcznie z tytanu. Idealne dla alergików.",
+            "text": "Kolczyki wykonane są ręcznie z tytanu. Idealne dla alergików i bardzo lekkie. Niesamowite kolory nadają tlenki tytanu wytworzone na powierzchni pod napięciem (anodowanie). Będą cieszyły latami. Ich kolor jest zależny od kąta padania światła. Długość całkowita kolczyków: 6,3 cm (długość liścia: 3,5 cm). Masa pojedynczego kolczyka: ok. 1,8 g (dla porównania moneta 5 groszy waży ok. 2,5 g) ",
+            "category": "Kolczyki",
+            "imgSrc": "./images/t1-1.jpg",
+            "id": 1,
+            "quantity": 1
+        },
+        {
+            "name": "Tytanowa broszka - Motyl",
+            "price": 135.00,
+            "description": "Broszka jest wykonana ręcznie z tytanu. Jest bardzo lekka, hipoalergiczna.",
+            "text": "Broszka jest wykonana ręcznie z tytanu. Jest bardzo lekka, hipoalergiczna. Motyl na ramieniu sprawi , że każdy strój stanie się radosny i kobiecy. Piękne kolory nadają tlenki tytanu, które będą cieszyły latami. Czubek igły nie wystaje poza zapięcie.Skrzydełka motylka wpisują się w prostokąt o wymiarach: 4,7 x 4 cm ",
+            "category": "Broszki",
+            "imgSrc": "./images/t2-1.jpg",
+            "id": 2,
+            "quantity": 1
+        },
+
+        {
+            "name": "Tytanowa bransoleta - Piwonia",
+            "price": 240.00,
+            "description": "Bransoletka wykonana ręcznie z tytanu.",
+            "text": "Bransoletka wykonana ręcznie z tytanu. Niesamowite kolory powstały dzięki pokryciu powierzchni tlenkami tytanu pod napięciem (anodowanie).Bransoleta jest hipoalergiczna, lekka i wytrzymała. Na kolejnych zdjęciach jest pokazana w zestawie z wisiorem na długim sznurku, z którym może stanowić komplet. Długość bransolety 14 cm + 2,8 cm szpary (bransoleta będzie dobra na nadgarstek o obwodzie ok 15 - 16 cm). W razie potrzeby bransoletkę można rozgiąć. Szerokość: 3,2 cm ",
+            "category": "Kolczyki",
+            "imgSrc": "./images/t3-1.jpg",
+            "id": 1,
+            "quantity": 1
+        },
+        
+
+
+    ])
+
+export default Products;

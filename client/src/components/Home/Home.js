aba79243bfffb8c3f29fc9bfdb8949d126770290
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-05-26 18:46:03 +0200

    Add leftMenu component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import PaginationComponent from 'react-reactstrap-pagination';
 
 //Import styles
-import './Home.scss'
+import './Home.scss';
 
 // Import products
 import Products from '../../assets/Products';
 
 // Import components
 import PorductCart from './ProductCart/ProductCart';
+import LeftMenu from './LeftMenu/LeftMenu';
 
 class Home extends Component {
   constructor(props) {
@@ -29,15 +30,16 @@ class Home extends Component {
       currentPage: index,
       currentProducts: [from, to]
     });
-    console.log(this.state.currentPage)
+    console.log(this.state.currentPage);
   }
 
   changeCategory(event) {
     const chosencategory = event.target.dataset.order;
-    const filtered = chosencategory !== "All"
-      ? Products.filter(item => item.category === chosencategory)
-      : Products
-    this.setState({ products: filtered })
+    const filtered =
+      chosencategory !== 'All'
+        ? Products.filter(item => item.category === chosencategory)
+        : Products;
+    this.setState({ products: filtered });
     this.handleSelected(1);
   }
 
@@ -45,49 +47,13 @@ class Home extends Component {
     return (
       <div className="home d-flex flex-md-row flex-column">
         <div className="col-md-4 col-12 justify-content-center">
-          <ul className="d-flex flex-column  mt-2">
-            <p>Kategorie:</p>
-            <li
-              className="nav-item"
-              data-prop='category'
-              data-order='All'
-              onClick={event => this.changeCategory(event)}>
-              Wszystkie
-                        </li>
-            <li
-              className="nav-item"
-              data-prop='category'
-              data-order='Kolczyki'
-              onClick={event => this.changeCategory(event)}>
-              Kolczyki
-                        </li>
-            <li
-              className="nav-item"
-              data-prop='category'
-              data-order='Pierścionki'
-              onClick={event => this.changeCategory(event)}>
-              Pierścionki
-                        </li>
-            <li
-              className="nav-item"
-              data-prop='category'
-              data-order='Bransoletki'
-              onClick={event => this.changeCategory(event)}>
-              Bransoletki
-                        </li>
-            <li
-              className="nav-item"
-              data-prop='category'
-              data-order='Broszki'
-              onClick={event => this.changeCategory(event)}>
-              Broszki
-                        </li>
-          </ul>
+          <LeftMenu
+            categories={[...new Set(Products.map(element=>element.category))]}
+            onChangeCategory={event => this.changeCategory(event)}
+          />
         </div>
         <div className="col-md-8 col-12 d-flex flex-wrap justify-content-start">
-          {this
-            .state
-            .products
+          {this.state.products
             .slice(this.state.currentProducts[0], this.state.currentProducts[1])
             .map((product, i) => {
               return (
@@ -97,19 +63,18 @@ class Home extends Component {
               );
             })}
           <div className="w-100 d-flex justify-content-center mt-auto">
-            {this.state.products.length > this.pageSize &&
-            <PaginationComponent
-              totalItems={this.state.products.length}
-              pageSize={this.pageSize}
-              onSelect={this
-                .handleSelected
-                .bind(this)}
-              activePage={this.state.currentPage}
-            />}
+            {this.state.products.length > this.pageSize && (
+              <PaginationComponent
+                totalItems={this.state.products.length}
+                pageSize={this.pageSize}
+                onSelect={this.handleSelected.bind(this)}
+                activePage={this.state.currentPage}
+              />
+            )}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
---
+++ client/src/components/Home/LeftMenu/LeftMenu.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './LeftMenu.scss';
+
+const LeftMenu = props => (
+    <ul className="left-menu d-flex flex-column mt-2">
+        <p>Kategorie:</p>
+        <li
+            className="nav-item"
+            data-prop="category"
+            data-order="All"
+            onClick={props.onChangeCategory}>
+            Wszystkie
+        </li>
+
+        {props
+            .categories
+            .map((item, i) => {
+                return (
+                    <li
+                        key={i}
+                        className="nav-item"
+                        data-prop="category"
+                        data-order={item}
+                        onClick={props.onChangeCategory}>
+                        {item}
+                    </li>
+                );
+            })}
+    </ul>
+);
+
+export default LeftMenu;

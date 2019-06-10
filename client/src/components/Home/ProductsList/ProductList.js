92d5b79a240399c185dc99de04a3d2580f4524aa
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-06-10 13:28:33 +0200

    Fix animation

--- client/src/components/Home/ProductsList/ProductList.js
+++ client/src/components/Home/ProductsList/ProductList.js
@@ -1,15 +1,20 @@
 import React from 'react';
-
 // Import components
 import PorductCard from './ProductCard/ProductCard';
+import Fade from 'react-reveal/Fade';
 
 const ProductList = props => (
-  <div className="d-flex flex-wrap justify-content-start ">
+  <div className="d-flex flex-wrap flex-row justify-content-start card-deck">
     {props.products.map((product, i) => {
       return (
-        <div className="p-2 col-md-6 col-12 " key={i}>
-          <PorductCard product={product} onAddToCartClic={(event)=>props.onAddToCartClic(product,event)} />
+        <Fade key={i}>
+        <div className="p-2 col-md-6 col-12 " >
+          <PorductCard
+            product={product}
+            onAddToCartClic={event => props.onAddToCartClic(product, event)}
+          />
         </div>
+        </Fade>
       );
     })}
   </div>

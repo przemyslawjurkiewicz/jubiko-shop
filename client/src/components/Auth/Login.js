e23a5401e43617a64a92d4775fe0d6af0ce34ed0
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-06-11 14:38:57 +0200

    Add component for auth

---
+++ client/src/components/Auth/Login.js
@@ -0,0 +1,130 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {loginUser} from '../../actions/authActions';
+import classnames from 'classnames';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      city: '',
+      street: '',
+      zip: '',
+      errors: {}
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/zamowienie'); // push user to order when  login
+    }
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
+  componentDidMount() {
+    // If logged in and user navigates to Login page, should redirect them to order
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/zamowienie');
+    }
+  }
+
+  onChange = e => {
+    this.setState({[e.target.id]: e.target.value});
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+      city: this.state.city,
+      street: this.state.street,
+      zip: this.state.zip
+    };
+    this.props.loginUser(userData);
+    console.log(userData);
+  };
+
+  render() {
+    const {errors} = this.state;
+    return (
+      <div className='container'>
+        <div className='row flex-column align-content-center justify-content-center'>
+          <div>
+            <h4>
+              <b>Zaloguj się</b>
+            </h4>
+            <p>
+              Nie posiadasz konta? <Link to='/register'>Zarejestruj się</Link>
+            </p>
+          </div>
+          <form noValidate onSubmit={this.onSubmit}>
+            <div className='col-sm-12 p-2'>
+              <input
+                onChange={this.onChange}
+                value={this.state.email}
+                error={errors.email}
+                id='email'
+                type='email'
+                placeholder='Email'
+                className={classnames('form-control', {
+                  'is-invalid': errors.email || errors.emailnotfound
+                })}
+              />
+              <small className='form-text text-danger'>
+                {errors.email}
+                {errors.emailnotfound}
+              </small>
+            </div>
+            <div className='col-sm-12 p-2'>
+              <input
+                onChange={this.onChange}
+                value={this.state.password}
+                error={errors.password}
+                id='password'
+                type='password'
+                placeholder='Hasło'
+                className={classnames('form-control', {
+                  'is-invalid': errors.password || errors.passwordincorrect
+                })}
+              />
+              <small className='form-text text-danger'>
+                {errors.password}
+                {errors.passwordincorrect}
+              </small>
+            </div>
+            <div className='col s12' style={{paddingLeft: '11.250px'}}>
+              <button ttype='submit' className='btn btn-primary'>
+                Logowanie
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  {loginUser}
+)(Login);
---
+++ client/src/components/Auth/Register.js
@@ -0,0 +1,209 @@
+import React, {Component} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {registerUser} from '../../actions/authActions';
+import classnames from 'classnames';
+import uuid from 'uuid';
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: uuid(),
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      city: '',
+      street: '',
+      zip: '',
+      errors: {}
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
+  componentDidMount() {
+    // If logged in and user navigates to Register page, should redirect them to order
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/zamowienie');
+    }
+  }
+
+  onChange = e => {
+    this.setState({[e.target.id]: e.target.value});
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const newUser = {
+      id: this.state.id,
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2,
+      city: this.state.city,
+      street: this.state.street,
+      zip: this.state.zip
+    };
+
+    this.props.registerUser(newUser, this.props.history);
+    console.log(newUser);
+  };
+
+  render() {
+    const {errors} = this.state;
+    return (
+      <div className='container'>
+        <div className='row flex-column align-content-center justify-content-center'>
+          <div>
+            <h4>
+              <b>Załóż konto</b>
+            </h4>
+            <p>
+              Posiadasz już konto? <Link to='/login'>Zaloguj się</Link>
+            </p>
+          </div>
+          <form noValidate onSubmit={this.onSubmit}>
+            <div className='col-sm-12 p-2'>
+              <input
+                onChange={this.onChange}
+                value={this.state.name}
+                error={errors.name}
+                id='name'
+                type='text'
+                placeholder='Imię i nazwisko'
+                className={classnames('form-control', {
+                  'is-invalid': errors.name
+                })}
+              />
+              <small className='form-text text-danger'>{errors.name}</small>
+            </div>
+            <div className='col-sm-12 p-2'>
+              <input
+                onChange={this.onChange}
+                value={this.state.email}
+                error={errors.email}
+                id='email'
+                type='email'
+                placeholder='Email'
+                className={classnames('form-control', {
+                  'is-invalid': errors.email
+                })}
+              />
+              <small className='form-text text-danger'>{errors.email}</small>
+            </div>
+            <div className='col-sm-12 p-2'>
+              <input
+                onChange={this.onChange}
+                value={this.state.password}
+                error={errors.password}
+                id='password'
+                type='password'
+                placeholder='Hasło'
+                className={classnames('form-control', {
+                  'is-invalid': errors.password
+                })}
+              />
+              <small className='form-text text-danger'>{errors.password}</small>
+            </div>
+            <div className='col-sm-12 p-2'>
+              <input
+                onChange={this.onChange}
+                value={this.state.password2}
+                error={errors.password2}
+                id='password2'
+                type='password'
+                placeholder='Potwierdź hasło'
+                className={classnames('form-control', {
+                  'is-invalid': errors.password2
+                })}
+              />
+              <small className='form-text text-danger'>
+                {errors.password2}
+              </small>
+            </div>
+            <div>
+              <h4>
+                <b>Adres wysyłki</b>
+              </h4>
+              <p>podaj adres do wysyłki:</p>
+            </div>
+            <div className='col-sm-12 p-2'>
+              <input
+                onChange={this.onChange}
+                value={this.state.street}
+                error={errors.street}
+                id='street'
+                type='text'
+                placeholder='Podaj ulicę i numer domu'
+                className={classnames('form-control', {
+                  'is-invalid': errors.street
+                })}
+              />
+              <small className='form-text text-danger'>{errors.street}</small>
+            </div>
+            <div className='form-row col-sm-12 p-2'>
+              <div className='col-7'>
+                <input
+                  onChange={this.onChange}
+                  value={this.state.city}
+                  error={errors.city}
+                  id='city'
+                  type='text'
+                  placeholder='Miasto'
+                  className={classnames('form-control', {
+                    'is-invalid': errors.city
+                  })}
+                />
+                <small className='form-text text-danger'>{errors.city}</small>
+              </div>
+              <div className='col'>
+                <input
+                  onChange={this.onChange}
+                  value={this.state.zip}
+                  error={errors.zip}
+                  id='zip'
+                  type='text'
+                  placeholder='Kod'
+                  className={classnames('form-control', {
+                    'is-invalid': errors.zip
+                  })}
+                />
+                <small className='form-text text-danger'>{errors.zip}</small>
+              </div>
+            </div>
+            <div>
+              <button type='submit' className='btn btn-primary'>
+                Zarejestruj
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+export default connect(
+  mapStateToProps,
+  {registerUser}
+)(withRouter(Register));

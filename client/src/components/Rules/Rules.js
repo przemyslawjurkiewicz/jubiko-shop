5091c76836d56dcb2bcda9210251912e1b2be18b
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-06-17 14:42:41 +0200

    Add rules component

---
+++ client/src/components/Rules/Rules.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+const Rules = () => (
+  <div className='faq d-flex flex-nowrap flex-column align-items-center justify-content-center col-10'>
+    <p>
+      Nam suscipit, risus metus eleifend in, ipsum. Curabitur et ultrices
+      posuere in, dapibus non, consectetuer adipiscing justo neque, euismod mi.
+      Fusce vel felis augue varius risus vehicula sit amet, varius nunc sem,
+      lobortis non, vehicula ut, diam. Ut elit justo ipsum feugiat lectus
+      rhoncus eu, eleifend et, faucibus ipsum dolor dictum sapien aliquet enim
+      nunc, lobortis facilisis. Maecenas at lorem nec libero. Pellentesque
+      scelerisque in, volutpat tempus enim. Cras luctus et malesuada augue a
+      laoreet hendrerit laoreet. Vestibulum fermentum. Vivamus semper risus.
+      Phasellus tempor magna suscipit enim. Phasellus fermentum, metus nec
+      tellus. Morbi pede. In nec elit ultricies nulla, faucibus.
+    </p>
+
+    <p>
+      Nullam fermentum diam a ligula. Lorem ipsum lorem eget velit. Pellentesque
+      dolor urna, id justo nibh vel dolor. Suspendisse eu lectus eget velit.
+      Aliquam quis enim. Nam laoreet iaculis arcu elementum eros porttitor ante
+      sodales sed, venenatis tristique, augue a dolor. Aliquam vitae pede. In
+      hac habitasse platea dictumst. Praesent elit metus, ullamcorper id, odio.
+      Ut sit amet mauris turpis, molestie turpis egestas. Mauris mattis a,
+      aliquet pulvinar, quam volutpat elit, sit amet, sollicitudin lorem,
+      iaculis malesuada euismod, quam sed turpis. Nullam consequat non, ultrices
+      posuere ante eget metus sed enim ut arcu. Nam rhoncus, dolor sit amet
+      sapien eros.
+    </p>
+
+    <p>
+      In non metus tellus, imperdiet wisi. Sed ultricies eu, fringilla eget,
+      neque. Vestibulum scelerisque. Vestibulum et lacus nulla luctus
+      scelerisque. Nam lorem tempus purus. Duis vulputate vehicula. Vivamus
+      iaculis, dui convallis at, mollis nulla ornare lorem. Phasellus
+      adipiscing. Nam ipsum. Nulla facilisis gravida, nisl ac nulla nulla, at
+      libero. Sed metus sed justo. Praesent faucibus. Sed bibendum. Nulla ipsum
+      pharetra ut, neque. Vestibulum dignissim tempor, sapien facilisis risus.
+      Aliquam in consequat porttitor. Nulla aliquet ipsum vel urna mattis vel,
+      ante. Praesent feugiat. Cum sociis natoque penatibus et ultrices posuere
+      sed, ullamcorper ac, eleifend quam nunc, accumsan sit amet, est. Integer.
+    </p>
+
+    <p>
+      Class aptent taciti sociosqu ad litora torquent per inceptos hymenaeos.
+      Maecenas blandit eros, ut tellus ac erat. Pellentesque ac eros quis nibh.
+      Phasellus facilisis libero. Nam ipsum. Nam suscipit, risus nec libero.
+      Morbi egestas, dui aliquam imperdiet. Nullam libero a dolor. Aliquam
+      adipiscing aliquet eu, nisl. Curabitur et wisi. Donec consectetuer
+      adipiscing elit. Lorem ipsum ut eros. Aliquam ultricies a, volutpat
+      aliquam purus. Phasellus at sapien. In hac habitasse platea dictumst. Cum
+      sociis natoque penatibus et netus et metus nisl at nunc justo ac libero.
+      In tempus. Pellentesque tortor. Integer id mi facilisis hendrerit. Sed
+      posuere sit amet, pretium porttitor.
+    </p>
+
+    <p>
+      Donec lectus semper eros. Fusce congue. Praesent nec augue. Donec nec leo
+      velit nulla in lacus pede, at ante. Sed dolor. Integer vestibulum lorem
+      lectus, volutpat tempus et, placerat scelerisque, ante ipsum ut leo lacus,
+      congue dolor. Quisque vehicula ullamcorper, enim non felis. Curabitur
+      placerat, egestas velit. Praesent et ligula. Nulla facilisi. Class aptent
+      taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+      hymenaeos. Pellentesque eu tempor vitae, pede. Lorem ipsum dolor placerat
+      augue. Nulla facilisi. Mauris vel molestie justo nibh nulla facilisis in,
+      suscipit congue sodales tempor. Suspendisse porttitor vitae, bibendum arcu
+      magna, at ante. In porttitor a.
+    </p>
+  </div>
+);
+
+export default Rules;

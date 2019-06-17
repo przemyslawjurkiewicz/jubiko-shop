a27b578e0cb970bced68aa48794d077bfea4dc47
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-06-17 14:42:05 +0200

    Add Contact component

---
+++ client/src/components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => (
+  <div className='faq d-flex flex-nowrap flex-column align-items-center justify-content-center col-10'>
+      <h2>Jubiko</h2>
+        <p>ul. Dziwna 23B/15</p>
+        <p>Ciekawe Miasto</p>
+        <p>tel. +48 500 500 200</p>
+  </div>
+);
+
+export default Contact;
